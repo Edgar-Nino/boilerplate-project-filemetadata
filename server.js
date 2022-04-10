2d5bd264d3e6d9b86c947a6ee9576476da8e30e6
Edgar Niño <edgarnio@live.com>
2022-04-10 18:20:32 -0500

    Initial Project

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' })
+
 require('dotenv').config()
 
 var app = express();
@@ -8,10 +13,18 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  try {
+    const { originalname, mimetype, size } = req.file
+    res.json({ name: originalname, type: mimetype, size: size });
+  }
+  catch (err) {
+    res.json({ error: err })
+  }
+})
 
 
 const port = process.env.PORT || 3000;
